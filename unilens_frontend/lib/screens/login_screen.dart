f6c4090f60840c1b92da7e5ea52cf51680624a21
Rahila <rahilaahmadi38@gmail.com>
2024-11-22 21:28:57 +0900

    working on page routes

--- unilens_frontend/lib/screens/login_screen.dart
+++ unilens_frontend/lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
