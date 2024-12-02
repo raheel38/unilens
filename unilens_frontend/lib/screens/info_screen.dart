d0858d3c3ae22714d70556d3752432748a265388
Rahila <rahilaahmadi38@gmail.com>
2024-12-02 16:48:31 +0900

    updated infoScreen

--- unilens_frontend/lib/screens/info_screen.dart
+++ unilens_frontend/lib/screens/info_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class InfoScreen extends StatefulWidget {
   const InfoScreen({super.key});
@@ -10,65 +8,8 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
-  List<dynamic> articles = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchArticles();
-  }
-
-  Future<void> fetchArticles() async {
-    const String url = 'http://localhost:8000/results';
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        setState(() {
-          articles = json.decode(response.body);
-          isLoading = false;
-        });
-      } else {
-        setState(() {
-          isLoading = false;
-        });
-      }
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Info Screen'),
-      ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                final article = articles[index];
-                return ListTile(
-                  title: Text(article['title'] ?? 'No Title'),
-                  onTap: () {
-                    _launchURL(article['url']);
-                  },
-                );
-              },
-            ),
-    );
-  }
-
-  void _launchURL(String url) async {
-    // Add url_launcher to handle URL redirection
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      print('Could not launch $url');
-    }
+    return const Placeholder();
   }
 }
